809dfecc3dbf0d6ee30a9e6e4edffa4dbe013050
seol-wonjun <qiops@naver.com>
2025-11-17 16:01:47 +0900

    Swagger and Cors Setting

---
+++ src/main/java/com/example/board/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.board;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 URL
+                        .allowedOrigins("http://localhost:3000") // 허용할 origin
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

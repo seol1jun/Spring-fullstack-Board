3035eb40ca31a49dc947ee9401e8f77e82e5461d
seol-wonjun <qiops@naver.com>
2025-11-24 12:34:54 +0900

    update detail part

--- frontend_board/src/pages/detail.jsx
+++ frontend_board/src/pages/detail.jsx
@@ -7,21 +7,29 @@ export default function Detail() {
   const { id } = useParams();
   const [post, setPost] = useState(null); //단일 게시물이기에 posts가 아닌 post임.
 
-  useEffect(() => { //얘로 서버에서 내용을 가져옴, useEffect는 페이지가 처음 열릴 때 한 번만 실행됨
-    axios.get(`http://localhost:8080/board/${id}`) //get의 api에게 요청을 보내서 데이터를 가져옴
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    axios.get(`http://localhost:8080/board/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
     .then((res) => {
         setPost(res.data);
     })
     .catch((err) => {
-        console.error("게시글 목록 조회 실패:", err);
-        alert("게시글 목록을 불러오는 데 실패했습니다.");
-    })
-  }, []);
+        console.error("게시글 상세 조회 실패:", err);
+        alert("게시글을 불러오는 데 실패했습니다.");
+    });
+
+}, [id]);
+
 
   const handleDelte = () => {
     const confirmed = window.confirm("정말로 이 글을 삭제하시나요?");
     if (confirmed) {
-      axios.delete(`http://localhost:8080/${id}`)
+      axios.delete(`http://localhost:8080/board/${id}`)
       .then(() => {
         alert(`ID ${id}번 글이 삭제되었습니다.`);
         window.location.href = "/" //nevigate 써도 되지않나? 나중에 한 번 써보기

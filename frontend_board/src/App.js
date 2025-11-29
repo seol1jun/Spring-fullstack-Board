8191ac6472803a8a3dc2b7d0ed1c69ddc598008c
seol-wonjun <qiops@naver.com>
2025-11-29 15:21:07 +0900

    update nevigate logic

--- frontend_board/src/App.js
+++ frontend_board/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Header from './components/header'
 import Home from "./pages/home";
 import Create from "./pages/create";
@@ -12,18 +12,27 @@ import './App.css'
 const App = () => {
   const location = useLocation();
 
-  // 로그인 페이지와 회원가입 페이지에서는 Header 숨기기
+  // 로그인 / 회원가입일 때는 헤더 숨김
   const hideHeader = location.pathname === "/login" || location.pathname === "/signup";
+
+  // 로그인 여부 체크 (토큰이 존재하는지)
+  const isLoggedIn = !!localStorage.getItem("token");
 
   return (
     <div>
       {!hideHeader && <Header />}
 
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path='/create' element={<Create/>}/>
-        <Route path='/board/:id' element={<Detail/>}/>
-        <Route path='/update/:id' element={<Update/>}/>
+        {/* "/" 접속 시: 로그인 X → login, 로그인 O → home */}
+        <Route 
+          path="/" 
+          element={isLoggedIn ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />} 
+        />
+
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create/>}/>
+        <Route path="/board/:id" element={<Detail/>}/>
+        <Route path="/update/:id" element={<Update/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
